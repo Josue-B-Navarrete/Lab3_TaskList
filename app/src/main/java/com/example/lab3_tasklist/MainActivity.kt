5d0fbda0d4de8ca0e054a853b29737f52712e9dd
Josue <Josuedrew88@gmail.com>
2022-04-17 15:11:04 -0700

    missing 3rd feature but it works

--- app/src/main/java/com/example/lab3_tasklist/MainActivity.kt
+++ app/src/main/java/com/example/lab3_tasklist/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.lab3_tasklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
-    private val stringArray = Array<String?>(5){""}
+    private val stringArray = Array<String?>(10){""}
     private lateinit var taskEditText: EditText
     private lateinit var addTaskBttn: Button
+    private lateinit var textviewList: TextView
     private var size = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,21 +25,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         addTaskBttn = findViewById(R.id.add_Button)
         taskEditText = findViewById(R.id.task_editText)
+        textviewList = findViewById(R.id.task_TextView2)
 
+        // Button Listener that adds a task to the list
         addTaskBttn.setOnClickListener {
             val taskString = taskEditText.text
-            if (size < 5) {
+            var tempString = ""
+            if (size < 10) {
                 stringArray[size] = taskString.toString()
+                tempString = stringArray[size] + "\n"
                 println("This is the array: " + stringArray[size])
                 size++
-            }
-            else {
+            } else {
                 val dialog = WarningDialogFragment()
                 dialog.show(supportFragmentManager, "warningDialog")
             }
+            textviewList.append(tempString)
         }
-    }
-
-
-
+        textviewList.setMovementMethod(ScrollingMovementMethod())
 }
+
+
+    }
