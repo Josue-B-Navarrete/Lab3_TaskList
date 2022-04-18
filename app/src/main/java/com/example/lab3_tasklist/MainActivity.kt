402de5195c1c164b69e62a63cb2a46bb12380ecf
Josue <Josuedrew88@gmail.com>
2022-04-18 10:11:09 -0700

    Attempted 3rd feature but it works

--- app/src/main/java/com/example/lab3_tasklist/MainActivity.kt
+++ app/src/main/java/com/example/lab3_tasklist/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.lab3_tasklist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.view.ContextMenu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-
+/**
+ * Main Activity of the program
+ *
+ * This class will host all of the actions/behaviours of the program.
+ *
+ * @author Josue B Navarrete
+ */
 class MainActivity : AppCompatActivity() {
+    // Variables needed
     private val stringArray = Array<String?>(10){""}
     private lateinit var taskEditText: EditText
     private lateinit var addTaskBttn: Button
@@ -20,9 +23,10 @@ class MainActivity : AppCompatActivity() {
     private var size = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Assigning the component variables to their ID
         addTaskBttn = findViewById(R.id.add_Button)
         taskEditText = findViewById(R.id.task_editText)
         textviewList = findViewById(R.id.task_TextView2)
@@ -31,19 +35,20 @@ class MainActivity : AppCompatActivity() {
         addTaskBttn.setOnClickListener {
             val taskString = taskEditText.text
             var tempString = ""
+            // Adds the task to the array
             if (size < 10) {
                 stringArray[size] = taskString.toString()
                 tempString = stringArray[size] + "\n"
-                println("This is the array: " + stringArray[size])
+                //Test
+                //println("This is the array: " + stringArray[size])
                 size++
             } else {
+                // Alert dialog with a button
                 val dialog = WarningDialogFragment()
                 dialog.show(supportFragmentManager, "warningDialog")
             }
+            // Append each line on to the textView
             textviewList.append(tempString)
         }
-        textviewList.setMovementMethod(ScrollingMovementMethod())
-}
-
-
     }
+}
--- app/src/main/java/com/example/lab3_tasklist/WarningDialogFrangment.kt
+++ app/src/main/java/com/example/lab3_tasklist/WarningDialogFrangment.kt
@@ -3,10 +3,6 @@ package com.example.lab3_tasklist
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
 class WarningDialogFragment : DialogFragment() {
